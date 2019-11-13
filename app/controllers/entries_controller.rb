7914685570f82267ace15969e4678684d5577e09
Josh <joshua.storrs19@gmail.com>
2019-11-13 13:47:14 -0500

    added word counter script to entries/new

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,13 @@
 class EntriesController < ApplicationController
     
+    def index
+        @entries = Entry.all
+    end
+
     def show
         @entry = Entry.find(params[:id])
     end
-    
+
     def new
     end
 
